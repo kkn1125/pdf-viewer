585a0e22715dbb2cb7f92e2ef4e0e65e5dfdbaa2
kkn1125 <xpfjrnseks1@naver.com>
2024-10-10 17:30:01 +0900

    🛠️ fix: .env 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,25 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { defineConfig, loadEnv } from "vite";
+import dotenv from "dotenv";
+import path from "path";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: "/",
-  plugins: [react(), tsconfigPaths()],
+export default defineConfig(({ command, mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+  const env = loadEnv(mode, process.cwd(), "");
+  const MODE = process.env.NODE_ENV || "production";
+  dotenv.config({
+    path: path.join(path.resolve(), ".env"),
+  });
+  dotenv.config({
+    path: path.join(path.resolve(), `.env.${MODE}`),
+  });
+  return {
+    // vite config
+    define: {
+      __APP_ENV__: JSON.stringify(env.APP_ENV),
+    },
+    plugins: [react(), tsconfigPaths()],
+  };
 });
